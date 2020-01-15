8472b1d325c95eb5a03b3f7a4172dcce9d121d15
Michael Anthony Leal Costa <MiCLeal@users.noreply.github.com>
2020-01-15 08:59:49 -0300

    Core

--- Core.lua
+++ Core.lua
@@ -5,7 +5,18 @@ local CreateFrame = CreateFrame
 local GetLocale = GetLocale
 local ReloadUI = ReloadUI
 local Settings = CKC.Settings
+local sFrame
 
-function CallSettings()
-    Settings.SettingsFrame.Show()
+function CreateSettingsFrame()
+    sFrame = CreateFrame("Frame", "CKCSettingsFame", UIParent)
+    sFrame:EnableMouse(true)
+    sFrame:SetMovable(true)
+    sFrame:RegisterForDrag("LeftButton")
+    sFrame:SetHeight(350)
+    sFrame:SetWidth(400)
+
+end
+local function ShowSettingsFrame()
+    if (not sFrame) then CreateSettingsFrame() end
+    sFrame:Show()
 end
